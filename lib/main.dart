2fbe30606a048a52c82dafe29e05be88b47a077d
hitnei <hoangnhi050298@gmail.com>
2020-12-22 15:13:15 +0700

    route + initState

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,14 @@
 import 'package:flutter/material.dart';
-import 'quotes.dart';
-import 'quote_card.dart';
+import 'package:myapp/pages/choose_location.dart';
+import 'package:myapp/pages/home.dart';
+import 'package:myapp/pages/loading.dart';
 
-void main() => runApp(MaterialApp(
-      home: QuoteList(),
+void main() =>
+    runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     ));
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  // List<String> quotes = [
-  //   'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text',
-  //   'op publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-  //   'and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-  // ];
-
-  List<Quote> quotes = [
-    Quote(
-        text:
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text',
-        author: 'abc xyz'),
-    Quote(
-        text:
-            'op publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-        author: 'abc xyz 123'),
-    Quote(
-        text:
-            'and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-        author: 'abc xyz 321'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
-      ),
-    );
-  }
-}
