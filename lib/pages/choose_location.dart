52ddad7ec1f6985f5e0eb27dc02246e5c6867c1c
hitnei <hoangnhi050298@gmail.com>
2020-12-22 16:29:57 +0700

    async await

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,16 +8,29 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
 
+  void getData() async {
+    // simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoo!';
+    });
+
+    // simulate network request for get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'vega, musician & egg collect!';
+    });
+
+    print('$username - $bio');
+  }
+
   @override
   void initState() {
     super.initState();
-    print('initState function ran');
+    getData();
+    print('after get data');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build function ran');
-
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
