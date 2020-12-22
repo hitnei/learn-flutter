f5a867d67f252c8f8f419fed907d5a3e26b9161e
hitnei <hoangnhi050298@gmail.com>
2020-12-22 10:29:06 +0700

    practice ninja ID card

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,94 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text("App Bar"),
+        title: Text('Ninja ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Image.asset('assets/space-01.jpg')
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.purple,
-              child: Text('1'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/chunli.jpg'),
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.blue,
-              child: Text('2'),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30),
-              color: Colors.amber,
-              child: Text('3'),
+            Text(
+              'NAME',
+              style: TextStyle(
+                fontSize: 18,
+                letterSpacing: 2,
+                color: Colors.grey[500],
+              ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('Click'),
-        onPressed: () {
-          print('Click!');
-        },
-        backgroundColor: Colors.red[600],
+            SizedBox(height: 10),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amber[200],
+                fontSize: 28,
+                letterSpacing: 2,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                fontSize: 18,
+                letterSpacing: 2,
+                color: Colors.grey[500],
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amber[200],
+                fontSize: 28,
+                letterSpacing: 2,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.mail_outline_outlined,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'chunli@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
